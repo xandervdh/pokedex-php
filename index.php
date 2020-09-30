f6419dbde70ee01ba9e3c179c1185ca666711081
xandervdh <xander-v.d.h@hotmail.com>
2020-09-30 11:03:52 +0200

    struggling with getting the last evolution

--- index.php
+++ index.php
@@ -1,9 +1,114 @@
 <?php
-//declare(strict_types = 1);
+declare(strict_types=1);
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
 error_reporting(E_ALL);
 
-$pokemon = file_get_contents("https://pokeapi.co/api/v2/pokemon/" . $_GET['id']);
-var_dump(json_decode($pokemon));
+if (isset($_POST['id'])){
+    $pokemon = $_POST['id'];
+} else {
+    $pokemon = 1;
+}
+
+function getData($url)
+{
+    $get = file_get_contents($url);
+    return json_decode($get, true);
+}
+
+$url = 'https://pokeapi.co/api/v2/pokemon/' . $pokemon;
+$data = getData($url);
+$idNumber = getId($data);
+$randMoves = getMoves($data);
+$evoChain = getEvolutions($data);
+var_dump($evoChain);
+
+function getId($data)
+{
+    if ($data['id'] < 10) {
+        return "#00" . $data['id'];
+    } else if ($data['id'] < 100) {
+        return "#0" . $data['id'];
+    } else {
+        return "#" . $data['id'];
+    }
+}
+
+function getMoves($data)
+{
+    $moves = array();
+    $max = count($data['moves']);
+    if ($max > 4) {
+        $maxMoves = 4;
+    } else {
+        $maxMoves = $max;
+    }
+    for ($i = 0; $i < $maxMoves; $i++) {
+        if ($max > 4) {
+            $rand = floor(rand(0, $max-1) - 0);
+            array_push($moves, $data['moves'][$rand]['move']['name']);
+        } else {
+            array_push($moves, $data['moves'][$i]['move']['name']);
+        }
+    }
+
+    return $moves;
+}
+
+function getEvolutions($data){
+    $speciesUrl = $data['species']['url'];
+    $species = getData($speciesUrl);
+    $evolutionUrl = $species['evolution_chain']['url'];
+    $evolutionChain = getData($evolutionUrl);
+    $evoData = $evolutionChain['chain'];
+    $evolveChain = array();
+
+    if (count($evoData['evolves_to']) > 1){
+        for ($i = 0; $i < count($evoData['evolves_to']); $i++){
+            array_push($evolveChain, $evoData['evolves_to'][$i]['species']['name']);
+        }
+    } else {
+        do {
+            array_push($evolveChain, $evoData['species']['name']);
+            $evoData = $evoData['evolves_to'][0];
+        } while (array_key_exists(0, $evoData['evolves_to']));
+        //array_push($evolveChain, $evoData['species']['name']);
+    }
+    return $evolveChain;
+}
+
+?>
+
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="index.php" method="post">
+    <label for="id">ID or Name: </label>
+    <input type="text" name="id" id="id"><br>
+    <input type="submit">
+</form>
+<div>
+    <strong><?php echo $data['name']; ?></strong><br>
+    <em><?php echo $idNumber ?></em>
+    <img src="<?php echo $data['sprites']['front_default'] ?>" alt=""><br>
+    <?php
+    $i = 0;
+    while ($i < count($randMoves)) {
+        echo '<strong>' . $randMoves[$i] . '</strong><br>';
+        $i++;
+    }
+    ?>
+</div>
+
+</body>
+</html>
